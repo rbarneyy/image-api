eae3b4be2b75ca6befe63368b68aa1b96bbcc1a7
rbarneyy <136350380+rbarneyy@users.noreply.github.com>
2023-06-12 19:03:50 +0200

    image gen v1

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+
+class ImageGenerator {
+  generateImage(prompt) {
+    return new Promise((resolve, reject) => {
+      // Generate the image based on the prompt
+      // Use any code or algorithms you like here
+      // For simplicity, let's just draw some random shapes on a canvas
+
+      const canvas = document.createElement('canvas');
+      canvas.width = 400;
+      canvas.height = 200;
+      const context = canvas.getContext('2d');
+
+      // Drawing random shapes
+      context.fillStyle = 'red';
+      context.fillRect(10, 10, 100, 100);
+
+      context.fillStyle = 'blue';
+      context.fillRect(150, 50, 200, 100);
+
+      // Export the image as a data URL
+      const dataURL = canvas.toDataURL();
+      
+      resolve(dataURL);
+    });
+  }
+}
+
+// Create an instance of the ImageGenerator
+const imageGenerator = new ImageGenerator();
+
+// Define the /image endpoint
+app.get('/image', (req, res) => {
+  const prompt = req.query.prompt || 'A colorful abstract painting';
+
+  imageGenerator.generateImage(prompt)
+    .then(dataURL => {
+      res.json({ image: dataURL });
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Failed to generate image' });
+    });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
