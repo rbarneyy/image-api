9f6066b0cc40dceb58a2cac8484a5439bacb209c
rbarneyy <136350380+rbarneyy@users.noreply.github.com>
2023-06-12 19:29:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const axios = require('axios');
+const { exec } = require('child_process');
 const app = express();
 const port = 3000;
 
 app.get('/randomimg', async (req, res) => {
   try {
+    await installPackage('axios'); // Install axios dynamically
+    const axios = require('axios'); // Import axios
+
     const response = await axios.get('https://source.unsplash.com/random');
     const imageUrl = response.request.res.responseUrl;
     res.json({ url: imageUrl });
@@ -13,6 +16,20 @@ app.get('/randomimg', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch random image' });
   }
 });
+
+function installPackage(packageName) {
+  return new Promise((resolve, reject) => {
+    exec(`npm install ${packageName}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error installing ${packageName}: ${error.message}`);
+        reject(error);
+      } else {
+        console.log(`Installed ${packageName}`);
+        resolve();
+      }
+    });
+  });
+}
 
 app.listen(port, () => {
   console.log(`API server listening at http://localhost:${port}`);
